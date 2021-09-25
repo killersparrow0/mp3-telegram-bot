04dfa265f9647273ed7b259060c0e04d795c4561
Milas <84676044+killersparrow0@users.noreply.github.com>
2021-09-25 13:59:05 +0530

    Update TG MP3 Download Bot.php

--- TG MP3 Download Bot.php
+++ TG MP3 Download Bot.php
@@ -1,5 +1,5 @@
 <?php
-$token = 'BOT_TOKEN';
+$token = '1844991724:AAHHejK4CsNPmnk0Bxst-gFQz328KhdVQ0s';
 $img = "BOT_IMG";
 
 $input = file_get_contents('php://input');
@@ -12,11 +12,11 @@ $lname = $update->message->chat->last_name;
 $inlinebutton = [
     'inline_keyboard' => [
         [
-            ['text' => "\xF0\x9F\x99\x8B Support Group", 'url' => 'https://t.me/sltechzoneofficial'],
-            ['text' => "\xF0\x9F\x94\x94 Update Channel", 'url' => 'https://t.me/sltechzone']
+            ['text' => "\xF0\x9F\x99\x8B Support Group", 'url' => 'https://t.me/movies_songs_tj'],
+            ['text' => "\xF0\x9F\x94\x94 Update Channel", 'url' => 'https://t.me/songs_movies']
         ],
         [
-            ['text' => "\xE2\x9E\x95 Add me to Your Group", 'url' => 'https://t.me/mp3downloadtgbot?startgroup=new']
+            ['text' => "\xE2\x9E\x95 Add me to Your Group", 'url' => 'https://t.me/Emmawatson_robot?startgroup=new']
         ],
     ]
 ];
@@ -51,14 +51,14 @@ if (strpos($send, "/mp3") === 0) {
 
 if (strpos($send, "/start") === 0) {
 
-    $welcometext = urlencode("<b>\xF0\x9F\x8E\xA7 Telegram MP3 Download Bot \xF0\x9F\x8E\xA7\n\n\xF0\x9F\x91\x8B Hey $fname $lname \xF0\x9F\x92\xAD I'm Telegram MP3 Download Bot i can Download Your MP3 Songs in Second \xF0\x9F\x9A\x80\n\n\xF0\x9F\x92\xAC Just Send me The Song Name You Want to Download\nEx - /mp3 alone\n\n\xE2\x9D\x93 Send /help to Get more Information About Telegram MP3 Download Bot \xE2\x9A\xA1\n\nDeveloped by @hirunaofficial \xf0\x9f\x87\xb1\xf0\x9f\x87\xb0</b>");
+    $welcometext = urlencode("<b>\xF0\x9F\x8E\xA7 Telegram MP3 Download Bot \xF0\x9F\x8E\xA7\n\n\xF0\x9F\x91\x8B Hey $fname $lname \xF0\x9F\x92\xAD I'm Telegram MP3 Download Bot i can Download Your MP3 Songs in Second \xF0\x9F\x9A\x80\n\n\xF0\x9F\x92\xAC Just Send me The Song Name You Want to Download\nEx - /s alone\n\n\xE2\x9D\x93 Send /help to Get more Information About Telegram MP3 Download Bot \xE2\x9A\xA1\n\nDeveloped by @tom_jerry_m \xf0\x9f\x87\xb1\xf0\x9f\x87\xb0</b>");
 
     file_get_contents("https://api.telegram.org/bot$token/sendphoto?chat_id=$chat_id&photo=$img&parse_mode=HTML&caption=$welcometext&reply_to_message_id=$msgid&reply_markup=$keyboard");
 }
 
 if (strpos($send, "/help") === 0) {
 
-	$helptext = urlencode("<b>\xF0\x9F\x8E\xA7 Telegram MP3 Download Bot \xF0\x9F\x8E\xA7\n\n\xE3\x80\xBD About Bot \xE3\x80\xBD\n\n\xE2\x96\xB6	Name - Telegram MP3 Download Bot\n\xE2\x96\xB6	Username - @mp3downloadtgbot\n\xE2\x96\xB6 Created By - @hirunaofficial\n\n\xF0\x9F\x94\xA7 Bot Commands \xF0\x9F\x94\xA7\n\n\xE2\x96\xB6	/start - Start Telegram MP3 Download Bot\n\xE2\x96\xB6	/help - More information about Telegram MP3 Download Bot\n\xE2\x96\xB6 /mp3 SONG_NAME - Download Your MP3 Songs\nEx - /mp3 alone</b>");
+	$helptext = urlencode("<b>\xF0\x9F\x8E\xA7 Telegram MP3 Download Bot \xF0\x9F\x8E\xA7\n\n\xE3\x80\xBD About Bot \xE3\x80\xBD\n\n\xE2\x96\xB6	Name - Telegram MP3 Download Bot\n\xE2\x96\xB6	Username - @Emmawatson_robot\n\xE2\x96\xB6 Created By - @tom_jerry_m\n\n\xF0\x9F\x94\xA7 Bot Commands \xF0\x9F\x94\xA7\n\n\xE2\x96\xB6	/start - Start Telegram MP3 Download Bot\n\xE2\x96\xB6	/help - More information about Telegram MP3 Download Bot\n\xE2\x96\xB6 /s SONG_NAME - Download Your MP3 Songs\nEx - /s alone</b>");
     
 	file_get_contents("https://api.telegram.org/bot$token/sendphoto?chat_id=$chat_id&photo=$img&parse_mode=HTML&caption=$helptext&reply_to_message_id=$msgid&reply_markup=$keyboard");
 }
